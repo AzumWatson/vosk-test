26e02141a0dc4a3a500eefe5b336f7b18f9cfd33
ruruko <ruruko@ruruko-no-mac.local>
2025-11-08 04:47:29 +0800

    Updates

---
+++ _app/immutable/chunks/BTNbXUjC.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_hlhpsj?.base??"/vosk-test",t=globalThis.__sveltekit_hlhpsj?.assets??s??"";export{t as a,s as b};
---
+++ _app/immutable/entry/start.Di8iEds-.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/COHBG2hR.js";export{o as load_css,r as start};
